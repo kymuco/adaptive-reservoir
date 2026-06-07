38e8aed2ddd5fcaa6982990196fbc2f0218c8921
Kymuco <ikymuco@gmail.com>
2026-06-07 14:58:48 +0600

    feat(core): export state diagnostics from core package

--- src/adaptive_reservoir/core/__init__.py
+++ src/adaptive_reservoir/core/__init__.py
@@ -16,6 +16,12 @@ from adaptive_reservoir.core.protocols import (
 from adaptive_reservoir.core.reservoir import ReservoirCore
 from adaptive_reservoir.core.result import AdaptiveChannels, AdaptiveStepResult, StepMetrics
 from adaptive_reservoir.core.state import ReservoirState
+from adaptive_reservoir.diagnostics import (
+    StateDiagnostics,
+    TraceNorms,
+    calculate_state_diagnostics,
+    rms_norm,
+)
 from adaptive_reservoir.features import extract_features
 
 __all__ = [
@@ -30,8 +36,12 @@ __all__ = [
     "ReservoirConfig",
     "ReservoirCore",
     "ReservoirState",
+    "StateDiagnostics",
     "StepMetrics",
     "TopologyBuilderProtocol",
     "TraceConfig",
+    "TraceNorms",
+    "calculate_state_diagnostics",
     "extract_features",
+    "rms_norm",
 ]
