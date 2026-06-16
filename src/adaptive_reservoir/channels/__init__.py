ddf618d6c5ddfa03e6d80c84cf82ee1c6e0e2ce7
Kymuco <ikymuco@gmail.com>
2026-06-16 17:45:03 +0600

    feat: export channel calculator snapshot

--- src/adaptive_reservoir/channels/__init__.py
+++ src/adaptive_reservoir/channels/__init__.py
@@ -1,5 +1,13 @@
 """Adaptive state channel calculators."""
 
-from adaptive_reservoir.channels.calculator import AdaptiveChannelCalculator
+from adaptive_reservoir.channels.calculator import (
+    CHANNEL_CALCULATOR_SNAPSHOT_SCHEMA_VERSION,
+    AdaptiveChannelCalculator,
+    ChannelCalculatorSnapshot,
+)
 
-__all__ = ["AdaptiveChannelCalculator"]
+__all__ = [
+    "AdaptiveChannelCalculator",
+    "CHANNEL_CALCULATOR_SNAPSHOT_SCHEMA_VERSION",
+    "ChannelCalculatorSnapshot",
+]
