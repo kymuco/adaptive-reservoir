99ff77a85ea4135318e037dc4e0138890b80c9ff
Kymuco <ikymuco@gmail.com>
2026-06-18 14:04:46 +0600

    feat: export delayed XOR benchmark

--- src/adaptive_reservoir/benchmarks/__init__.py
+++ src/adaptive_reservoir/benchmarks/__init__.py
@@ -2,10 +2,12 @@
 
 from adaptive_reservoir.benchmarks.common import BenchmarkResult
 from adaptive_reservoir.benchmarks.concept_drift import run_concept_drift_benchmark
+from adaptive_reservoir.benchmarks.delayed_xor import run_delayed_xor_benchmark
 from adaptive_reservoir.benchmarks.temporal_drift import run_temporal_drift_benchmark
 
 __all__ = [
     "BenchmarkResult",
     "run_concept_drift_benchmark",
+    "run_delayed_xor_benchmark",
     "run_temporal_drift_benchmark",
 ]
