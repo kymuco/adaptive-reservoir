b1a5128cd766605401f8a952436fc9174c2b81e7
Kymuco <ikymuco@gmail.com>
2026-06-18 13:49:28 +0600

    feat: export temporal drift benchmark

--- src/adaptive_reservoir/benchmarks/__init__.py
+++ src/adaptive_reservoir/benchmarks/__init__.py
@@ -2,8 +2,10 @@
 
 from adaptive_reservoir.benchmarks.common import BenchmarkResult
 from adaptive_reservoir.benchmarks.concept_drift import run_concept_drift_benchmark
+from adaptive_reservoir.benchmarks.temporal_drift import run_temporal_drift_benchmark
 
 __all__ = [
     "BenchmarkResult",
     "run_concept_drift_benchmark",
+    "run_temporal_drift_benchmark",
 ]
