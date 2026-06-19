a9172cbf1f6e77a5578c7183de4f96f240f6b1e3
Kymuco <ikymuco@gmail.com>
2026-06-19 16:25:24 +0600

    feat(experimental): export Oja compressor from experimental namespace

--- src/adaptive_reservoir/experimental/__init__.py
+++ src/adaptive_reservoir/experimental/__init__.py
@@ -1,9 +1,16 @@
 """Experimental adaptive-reservoir algorithms."""
 
+from adaptive_reservoir.experimental.oja import OjaCompressor, OjaCompressorSnapshot
 from adaptive_reservoir.experimental.rls import RLSReadout
 from adaptive_reservoir.experimental.sparse_readout import (
     SPARSE_ONLINE_READOUT_NAME,
     SparseOnlineReadout,
 )
 
-__all__ = ["RLSReadout", "SPARSE_ONLINE_READOUT_NAME", "SparseOnlineReadout"]
+__all__ = [
+    "OjaCompressor",
+    "OjaCompressorSnapshot",
+    "RLSReadout",
+    "SPARSE_ONLINE_READOUT_NAME",
+    "SparseOnlineReadout",
+]
