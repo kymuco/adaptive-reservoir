67a572cef9f86339cc5f1281005ea1a43527fe95
Kymuco <ikymuco@gmail.com>
2026-06-19 16:06:25 +0600

    feat(experimental): export sparse online readout from experimental namespace

--- src/adaptive_reservoir/experimental/__init__.py
+++ src/adaptive_reservoir/experimental/__init__.py
@@ -1,5 +1,9 @@
 """Experimental adaptive-reservoir algorithms."""
 
 from adaptive_reservoir.experimental.rls import RLSReadout
+from adaptive_reservoir.experimental.sparse_readout import (
+    SPARSE_ONLINE_READOUT_NAME,
+    SparseOnlineReadout,
+)
 
-__all__ = ["RLSReadout"]
+__all__ = ["RLSReadout", "SPARSE_ONLINE_READOUT_NAME", "SparseOnlineReadout"]
